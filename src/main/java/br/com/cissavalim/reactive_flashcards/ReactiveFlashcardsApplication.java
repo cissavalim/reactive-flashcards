d0b9bb235606668898e8739bf522b7b7234cfd5f
Cecilia Valim <cissamadalenarv@gmail.com>
2025-01-08 21:07:39 -0300

    first commit

---
+++ src/main/java/br/com/cissavalim/reactive_flashcards/ReactiveFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.reactive_flashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cissavalim/reactive_flashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cissavalim.reactive_flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
